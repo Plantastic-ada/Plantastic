2740ebe4defb2a4b25c4bf52836d8db02542403d
nathanc8 <cazard.nathan@gmail.com>
2025-12-19 15:44:42 +0100

    resolve merge conflicts

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -19,75 +19,65 @@ export const Header = () => {
 		setDropDownIsOpen(!dropDownIsOpen);
 	};
 	return (
-		<div className="sticky top-0 z-20 bg-transparent  flex justify-between items-center h-10 pointer-events-none  ">
-			{/* <div id="header"> */}
+		<div className="sticky top-0 z-20 bg-transparent  flex justify-between items-center h-10">
 			<div>
 				<img
 					onClick={() => navigate("/")}
 					src={logo}
 					className="w-24 h-24 ml-4
-          pointer-events-auto cursor-pointer
           sm:w-24 sm:h-30 lg:w-48 sm:ml-5
           md: md:ml-6
           lg: lg:ml-1
           2xl:h-48 2xl:ml-6 
           object-contain"
 				/>
-        <button>
-          <LuDroplets
-            color="white"
-            onClick={() => setIsModalOpen(true)}
-            className="
+				<button>
+					<LuDroplets
+						color="white"
+						onClick={() => setIsModalOpen(true)}
+						className="
             w-14 h-14 mr-6
           sm:w-16 sm:h-16 sm:mr-4 
           md:w-14 md:h-14 md:mr-8
           2xl:w-24  2xl:h-24  2xl:mr-10"
-          />
-        </button>
-        <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-          <WateringModal onClose={() => setIsModalOpen(false)} />
-        </Modal>
-      </div>
-      {dropDownIsOpen && (
-        <div
-          className="origin-top-right absolute right-0  w-56 rounded-md shadow-lg bg-amber-50/95 font-bellota ring-black ring-opacity-5 focus:outline-none z-30"
-          role="menu"
-        >
-          <div className="py-1" role="none">
-            <a
-              href="#"
-              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-300"
-              role="menuitem"
-            >
-              Profile
-            </a>
-            <a
-              href="#"
-              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-300"
-              role="menuitem"
-            >
-              Settings
-            </a>
-            <button
-              onClick={logout}
-              className="block px-4 py-2 mt-4 bg-red-500 text-white rounded hover:bg-red-600 disabled:opacity-50"
-            >
-              Logout
-            </button>
-          </div>
-        </div>
-      )}
-      <div>
-        <button className=""></button>
+					/>
+				</button>
+				<Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+					<WateringModal onClose={() => setIsModalOpen(false)} />
+				</Modal>
+			</div>
+			{dropDownIsOpen && (
+				<div
+					className="origin-top-right absolute right-0  w-56 rounded-md shadow-lg bg-amber-50/95 font-bellota ring-black ring-opacity-5 focus:outline-none z-30"
+					role="menu"
+				>
+					<div className="py-1" role="none">
+						<a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-300" role="menuitem">
+							Profile
+						</a>
+						<a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-300" role="menuitem">
+							Settings
+						</a>
+						<button
+							onClick={logout}
+							className="block px-4 py-2 mt-4 bg-red-500 text-white rounded hover:bg-red-600 disabled:opacity-50"
+						>
+							Logout
+						</button>
+					</div>
+				</div>
+			)}
+			<div>
+				<button className="">
 					<PiUserCircleGearFill
-           onClick={toogleDropDown}
+						onClick={toogleDropDown}
 						color="white"
 						className="w-14 h-14 mr-6
-          sm:w-16 sm:h-16 sm:mr-4 
-          md:w-14 md:h-14 md:mr-8
-          2xl:w-24  2xl:h-24  2xl:mr-10"
+           sm:w-16 sm:h-16 sm:mr-4 
+           md:w-14 md:h-14 md:mr-8
+           2xl:w-24  2xl:h-24  2xl:mr-10"
 					/>
-				</div>
+				</button>
 			</div>
 		</div>
 	);
