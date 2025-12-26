1a9083a0cce200ec0e60ddc2ea41c166b0a2efa1
Yennie <yennielake@gmail.com>
2025-12-26 15:00:19 +0100

    PWA functional, again :)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,14 +10,27 @@ import GardenProvider from "./context/GardenContext";
 
 const root = document.getElementById("root")!;
 ReactDOM.createRoot(root).render(
-	// MUST REMOVE <React.StrictMode> TO DEPLOY
-	<React.StrictMode>
-		<BrowserRouter>
-			<AuthProvider>
-				<GardenProvider>
-					<App />
-				</GardenProvider>
-			</AuthProvider>
-		</BrowserRouter>
-	</React.StrictMode>
+  // MUST REMOVE <React.StrictMode> TO DEPLOY
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <GardenProvider>
+          <App />
+        </GardenProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) => {
+        console.log("SW registered:", registration);
+      })
+      .catch((error) => {
+        console.log("SW registration failed:", error);
+      });
+  });
+}
