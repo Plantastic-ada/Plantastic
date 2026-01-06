7011651bb261df559c1fef42d210ddaf629c2037
Yennie <yennielake@gmail.com>
2026-01-06 17:18:22 +0100

    CI fix

--- frontend/src/mocks/handlers/plantHandlers.ts
+++ frontend/src/mocks/handlers/plantHandlers.ts
@@ -1,7 +1,6 @@
 import { http, HttpResponse } from "msw";
 import { TEST_CONFIG } from "../config";
 import { mockPlants } from "../mockPlantData";
-import { mockUserPlants } from "../mockUserPlantsData";
 
 export const plantSummariesHandlers = [
   http.get(`${TEST_CONFIG.API_BASE_URL}/api/plants/summaries`, () => {
