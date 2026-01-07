bf80b0c5e21a8a6406c77b56d37613fefe1ab518
nathanc8 <cazard.nathan@gmail.com>
2026-01-07 16:14:28 +0100

    change app properties

--- backend/src/main/java/com/plantastic/backend/security/config/CorsConfig.java
+++ backend/src/main/java/com/plantastic/backend/security/config/CorsConfig.java
@@ -1,16 +1,22 @@
 package com.plantastic.backend.security.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Slf4j
 public class CorsConfig implements WebMvcConfigurer {
+    @Value("${frontend_url}")
+    String frontendUrl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+      log.info("frontend Url : {}", frontendUrl);
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
