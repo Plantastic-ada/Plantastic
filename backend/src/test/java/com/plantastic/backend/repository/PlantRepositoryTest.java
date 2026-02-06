1d986d76f075fc1469a7dff2f6c70a69ceddd520
nathanc8 <cazard.nathan@gmail.com>
2026-02-06 20:02:32 +0100

    add profile to repositories tests

--- backend/src/test/java/com/plantastic/backend/repository/PlantRepositoryTest.java
+++ backend/src/test/java/com/plantastic/backend/repository/PlantRepositoryTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,6 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @EntityScan("com.plantastic.backend.models")
+@ActiveProfiles("test")
 class PlantRepositoryTest {
 
   @Autowired
--- backend/src/test/java/com/plantastic/backend/repository/UserPlantRepositoryTest.java
+++ backend/src/test/java/com/plantastic/backend/repository/UserPlantRepositoryTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -20,6 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @EntityScan("com.plantastic.backend.models")
+@ActiveProfiles("test")
 class UserPlantRepositoryTest {
 
   @Autowired
